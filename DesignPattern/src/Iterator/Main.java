cea11ff11a06d954ca91bad4ff3e41ee0452574e
devsik <devsik@192.168.8.27>
2013-03-10 00:41:51 +0900

    Iterator pattern

---
+++ DesignPattern/src/Iterator/Main.java
@@ -0,0 +1,18 @@
+package Iterator;
+
+public class Main {
+	public static void main(String[] args) {
+		BookShelf bookShelf = new BookShelf(4);
+		bookShelf.appendBook(new Book("책1"));
+		bookShelf.appendBook(new Book("책2"));
+		bookShelf.appendBook(new Book("책3"));
+		bookShelf.appendBook(new Book("책4"));
+		
+		java.util.Iterator it = bookShelf.iterator();
+		
+		while (it.hasNext()) {
+			Book book = (Book)it.next();
+			System.out.println(book.getName());
+		}
+	}
+}
